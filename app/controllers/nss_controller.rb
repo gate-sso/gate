03055f2c29efd0e39ef408bb98362ea381aa3e40
Ajey Gore <ajeygore@gmail.com>
2016-08-01 21:58:16 +0530

    [ajey] reverted to old set and expire function for redis cache

--- app/controllers/nss_controller.rb
+++ app/controllers/nss_controller.rb
@@ -22,7 +22,8 @@ class NssController < ApplicationController
         @response = REDIS_CACHE.get(GROUP_NAME_RESPONSE + name)
         if @response.blank?
           @response = Group.get_name_response(name).to_json
-          REDIS_CACHE.set(GROUP_NAME_RESPONSE + name, @response, REDIS_KEY_EXPIRY)
+          REDIS_CACHE.set(GROUP_NAME_RESPONSE + name, @response)
+          REDIS_CACHE.expire(GROUP_NAME_RESPONSE + name, REDIS_KEY_EXPIRY)
         end
       end
 
@@ -31,7 +32,8 @@ class NssController < ApplicationController
         @response = REDIS_CACHE.get(GROUP_GID_RESPONSE + gid)
         if @response.blank?
           @response = Group.get_gid_response(gid).to_json
-          REDIS_CACHE.set(GROUP_GID_RESPONSE + gid, @response, REDIS_KEY_EXPIRY)
+          REDIS_CACHE.set(GROUP_GID_RESPONSE + gid, @response)
+          REDIS_CACHE.expire(GROUP_GID_RESPONSE + gid, REDIS_KEY_EXPIRY)
         end
 
       end
@@ -40,7 +42,8 @@ class NssController < ApplicationController
         @response = REDIS_CACHE.get(GROUP_ALL_RESPONSE)
         if @response.blank?
           @response = Group.get_all_response.to_json
-          REDIS_CACHE.set(GROUP_ALL_RESPONSE, @response, REDIS_KEY_EXPIRY)
+          REDIS_CACHE.set(GROUP_ALL_RESPONSE, @response)
+          REDIS_CACHE.expire(GROUP_ALL_RESPONSE, REDIS_KEY_EXPIRY)
         end
       end
     end
@@ -58,13 +61,15 @@ class NssController < ApplicationController
         @response = REDIS_CACHE.get(SHADOW_NAME_RESPONSE + name)
         if @response.blank?
           @response = User.get_shadow_name_response(name).to_json
-          REDIS_CACHE.set(SHADOW_NAME_RESPONSE + name, @response, REDIS_KEY_EXPIRY)
+          REDIS_CACHE.set(SHADOW_NAME_RESPONSE + name, @response)
+          REDIS_CACHE.expire(SHADOW_NAME_RESPONSE + name, REDIS_KEY_EXPIRY)
         end
       else
         @response = REDIS_CACHE.get(SHADOW_ALL_RESPONSE)
         if @response.blank?
           @response = User.get_all_shadow_response.to_json
-          REDIS_CACHE.set(SHADOW_ALL_RESPONSE, @response, REDIS_KEY_EXPIRY)
+          REDIS_CACHE.set(SHADOW_ALL_RESPONSE, @response)
+          REDIS_CACHE.expire(SHADOW_ALL_RESPONSE, REDIS_KEY_EXPIRY)
         end
       end
     end
@@ -82,7 +87,8 @@ class NssController < ApplicationController
         @response = REDIS_CACHE.get(PASSWD_NAME_RESPONSE + name)
         if @response.blank?
           @response = User.get_passwd_name_response(name).to_json
-          REDIS_CACHE.set(PASSWD_NAME_RESPONSE + name, @response, REDIS_KEY_EXPIRY)
+          REDIS_CACHE.set(PASSWD_NAME_RESPONSE + name, @response)
+          REDIS_CACHE.expire(PASSWD_NAME_RESPONSE + name, REDIS_KEY_EXPIRY)
         end
       end
       uid = params[:uid]
@@ -90,7 +96,8 @@ class NssController < ApplicationController
         @response = REDIS_CACHE.get(PASSWD_UID_RESPONSE + uid)
         if @response.blank?
           @response = User.get_passwd_uid_response(uid).to_json
-          REDIS_CACHE.set(PASSWD_UID_RESPONSE + uid, @response, REDIS_KEY_EXPIRY)
+          REDIS_CACHE.set(PASSWD_UID_RESPONSE + uid, @response)
+          REDIS_CACHE.expire(PASSWD_UID_RESPONSE + uid, REDIS_KEY_EXPIRY)
         end
       end
 
@@ -98,7 +105,8 @@ class NssController < ApplicationController
         @response = REDIS_CACHE.get(PASSWD_ALL_RESPONSE)
         if @response.blank?
           @response = User.get_all_passwd_response.to_json
-          REDIS_CACHE.set(PASSWD_ALL_RESPONSE, @response, REDIS_KEY_EXPIRY)
+          REDIS_CACHE.set(PASSWD_ALL_RESPONSE, @response)
+          REDIS_CACHE.expire(PASSWD_ALL_RESPONSE, REDIS_KEY_EXPIRY)
         end
       end
       render json: @response
