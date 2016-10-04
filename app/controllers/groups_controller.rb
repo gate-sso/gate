77982a8a9c55cbd95e48b4da1e0507a891c0c9d5
Ajey Gore <ajeygore@gmail.com>
2016-10-04 20:17:26 +0530

    [ajey] Adding views for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,37 @@
 class GroupsController < ApplicationController
   def index
 
+    @groups = Group.all
+
   end
 
   def create
+    @group = Group.new(group_params)
+     respond_to do |format|
+      if @group.save
+      format.html { redirect_to groups_path, notice: 'Group was successfully created.' }
+      format.json { render status: :created, json: "#{@group.name}host created" }
+      else
+        format.html { redirect_to groups_path, notice: "Can't save '#{group_params[:name]}'" }
+      format.json { render status: :error, json: "#{@group.name} not created" }
+      end
+    end
+
+
 
   end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = group.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
+
 
 end
