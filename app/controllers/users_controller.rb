2e31a64f19b3bd02771b7e413aebb439886133e1
Ajey Gore <ajeygore@gmail.com>
2017-10-04 13:54:24 +0530

    [ajey] added users controller migration from profile admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
 
   def index
+    @user_search = params[:user_search]
+    @users = []
+    @users = User.where("name like ?", "%#{@user_search}%").take(20) if @user_search.present?
+  end
+
+  def show
 
   end
+
+  def update
+
+  end
+
+  def create
+
+  end
+  
 end
