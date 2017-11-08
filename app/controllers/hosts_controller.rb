ad0c8f0e8d4e8d0b3bcd013ed62377ffe4128c7e
Akshat|Ujwalendu <akshat.shah@go-jek.com>
2017-11-08 14:38:29 +0530

    Give host pattern access to a list of users

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,15 +1,12 @@
-class HostsController < ApplicationController
-  before_filter :authenticate_access_token!
+class HostsController < ActionController::Base
   def create
+    head :unauthorized if !(AccessToken.valid_token params[:token])
     users_email_list = params[:users_list].split(',')
 
     users_email_list.each do |user_email|
       @user = User.find_active_user_by_email(user_email)
       next if @user.nil?
-      host = Host.new
-      host.user = @user
-      host.host_pattern = params[:host_pattern]
-      host.save!
+      host = Host.find_or_create_by(user: @user, host_pattern: params[:host_pattern])
     end
 
     head :created
