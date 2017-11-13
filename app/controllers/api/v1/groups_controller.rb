4ce868cce099173dffafb59c52c803240fbdde3c
Akshat|Ujwalendu <akshat.shah@go-jek.com>
2017-11-13 14:44:43 +0530

    Add list of vpns to a group
    - Feature only for super admin

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -19,5 +19,23 @@ class ::Api::V1::GroupsController < ApiController
 
     render plain: "List of users added"
   end
+
+  def add_vpns_list
+    render plain: "api only authorized for super admins" and return unless current_user.admin?
+
+    @group = Group.find_by_name(params[:group_name])
+    render plain: "Group not found" and return if @group.nil?
+
+    vpn_ip_address_list = params[:vpn_ip_address_list].split(',')
+    vpn_ip_address_list.each do |vpn_ip_address|
+      @vpn = Vpn.find_by_ip_address(vpn_ip_address)
+      next if @vpn.nil?
+
+      @vpn.groups << @group if @vpn.groups.find_by_id(@group.id).blank?
+      @vpn.save!
+    end
+
+    render plain: "Group has access to list of vpns"
+  end
 end
 
