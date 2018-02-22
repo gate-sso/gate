918c3f6ea82456c96f2f8d041a816df9cd8421cb
Ajey Gore <ajeygore@gmail.com>
2018-02-23 05:09:35 +0700

    [ajey] remove caches from groups_controller and added count to group members in group show view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -149,26 +149,6 @@ class GroupsController < ApplicationController
       @group = Group.find(params[:group_id])
       @user.groups << @group if @user.groups.find_by_id(params[:group_id]).blank?
       @user.save! 
-      REDIS_CACHE.del(PASSWD_NAME_RESPONSE + @user.email.split('@').first)
-      REDIS_CACHE.del(SHADOW_NAME_RESPONSE + @user.email.split('@').first)
-      REDIS_CACHE.del(PASSWD_UID_RESPONSE + @user.uid.to_s)
-
-      @user.groups.each do |group|
-        REDIS_CACHE.del(GROUP_NAME_RESPONSE + group.name)
-        REDIS_CACHE.del(GROUP_GID_RESPONSE + group.gid.to_s)
-      end
-
-
-      @response = Group.get_all_response.to_json
-      REDIS_CACHE.set(GROUP_ALL_RESPONSE, @response)
-      REDIS_CACHE.expire(GROUP_ALL_RESPONSE, REDIS_KEY_EXPIRY)
-      @response = User.get_all_shadow_response.to_json
-      REDIS_CACHE.set(SHADOW_ALL_RESPONSE, @response)
-      REDIS_CACHE.expire(SHADOW_ALL_RESPONSE, REDIS_KEY_EXPIRY)
-      @response = User.get_all_passwd_response.to_json
-      REDIS_CACHE.set(PASSWD_ALL_RESPONSE, @response)
-      REDIS_CACHE.expire(PASSWD_ALL_RESPONSE, REDIS_KEY_EXPIRY)
-
 
     end
     redirect_to user_path
@@ -180,25 +160,7 @@ class GroupsController < ApplicationController
       group = Group.find(params[:id])
 
       if @user.email.split('@').first != group.name
-        @user.groups.each do |user_group|
-          REDIS_CACHE.del(GROUP_NAME_RESPONSE + user_group.name)
-          REDIS_CACHE.del(GROUP_GID_RESPONSE + user_group.gid.to_s)
-        end
         @user.groups.delete(group)
-        REDIS_CACHE.del(PASSWD_NAME_RESPONSE + @user.email.split('@').first)
-        REDIS_CACHE.del(SHADOW_NAME_RESPONSE + @user.email.split('@').first)
-        REDIS_CACHE.del(PASSWD_UID_RESPONSE + @user.uid.to_s)
-
-        @response = Group.get_all_response.to_json
-        REDIS_CACHE.set(GROUP_ALL_RESPONSE, @response)
-        REDIS_CACHE.expire(GROUP_ALL_RESPONSE, REDIS_KEY_EXPIRY)
-        @response = User.get_all_shadow_response.to_json
-        REDIS_CACHE.set(SHADOW_ALL_RESPONSE, @response)
-        REDIS_CACHE.expire(SHADOW_ALL_RESPONSE, REDIS_KEY_EXPIRY)
-        @response = User.get_all_passwd_response.to_json
-        REDIS_CACHE.set(PASSWD_ALL_RESPONSE, @response)
-        REDIS_CACHE.expire(PASSWD_ALL_RESPONSE, REDIS_KEY_EXPIRY)
-
       end
 
     end
