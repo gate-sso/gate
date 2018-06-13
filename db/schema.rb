d2414862f52fb825fa4cd8389d7f672bf4e31869
Siddarth R <siddarth.ramaswamy@go-jek.com>
2018-06-13 22:22:32 +0530

    Removed Old SAML Code

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180613074108) do
+ActiveRecord::Schema.define(version: 20180613165050) do
 
   create_table "access_tokens", force: :cascade do |t|
     t.string   "hashed_token", limit: 255
@@ -117,14 +117,6 @@ ActiveRecord::Schema.define(version: 20180613074108) do
     t.string   "name",         limit: 255
     t.string   "url",          limit: 255
     t.string   "email_domain", limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
-  create_table "saml_service_providers", force: :cascade do |t|
-    t.string   "name",         limit: 255, null: false
-    t.string   "sso_url",      limit: 255, null: false
-    t.string   "metadata_url", limit: 255, null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
--- spec/factories/saml_service_providers.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :saml_service_provider do
-    
-  end
-end
