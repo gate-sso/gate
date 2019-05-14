f374cc158295f50ab7dd0637c1a2bb69374138de
William Albertus Dembo <william.dembo@aux.go-jek.com>
2019-05-14 10:27:55 +0700

    sign in only allow authorized domain

--- app/controllers/users/auth_controller.rb
+++ app/controllers/users/auth_controller.rb
@@ -2,6 +2,12 @@ class Users::AuthController < ApplicationController
   before_action :set_paper_trail_whodunnit
 
   def sign_in
+    email = params.require(:email)
+
+    unless User.valid_domain? email.split('@').last
+      return render plain: 'Your domain is unauthorized', status: :unauthorized
+    end
+
     redirect_to root_path
   end
 end
--- spec/controllers/users/auth_controller_spec.rb
+++ spec/controllers/users/auth_controller_spec.rb
@@ -4,10 +4,26 @@ RSpec.describe Users::AuthController, type: :controller do
   let(:user) { FactoryBot.create(:user, name: 'foobar', email: 'foobar@foobar.com') }
 
   context 'sign in' do
+    before(:each) do
+      @cached_domain_env = Figaro.env.GATE_HOSTED_DOMAINS
+      ENV['GATE_HOSTED_DOMAINS'] = 'foobar.com'
+    end
+
+    after(:each) do
+      ENV['GATE_HOSTED_DOMAINS'] = @cached_domain_env
+    end
+
     it 'should redirect to home when success' do
       post :sign_in, params: { name: user.name, email: user.email }
 
       expect(response).to redirect_to(root_path)
+    end
+
+    it 'should produce error and unauthorized status when email domain is unsupported' do
+      post :sign_in, params: { name: user.name, email: 'foobar@notfoobar.com' }
+
+      expect(response).to have_http_status(401)
+      expect(response.body).to eq('Your domain is unauthorized')
     end
   end
 end
