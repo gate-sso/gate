852513131b69f93189381853f2dbc2c0ab44076d
William Albertus Dembo <william.dembo@aux.go-jek.com>
2019-05-14 14:29:57 +0700

    home view will render sign in form

--- spec/views/layouts/home.html.slim_spec.rb
+++ spec/views/layouts/home.html.slim_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe 'layouts/home', type: :view do
 
     assert_select "a[href$='#{user_google_oauth2_omniauth_authorize_path}']"
   end
+
+  it 'renders sign in form' do
+    render
+
+    assert_select 'form[action=?][method=?]', user_sign_in_path, 'post' do
+      assert_select 'input#name[name=name]'
+      assert_select 'input#email[name=email]'
+    end
+  end
 end
