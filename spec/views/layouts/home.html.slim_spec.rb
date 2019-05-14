3adabdbedff2c0894c94ab3cda6bd081852926ee
William Albertus Dembo <william.dembo@aux.go-jek.com>
2019-05-14 15:08:03 +0700

    home view will render sign in form only if sign in type is form

--- spec/views/layouts/home.html.slim_spec.rb
+++ spec/views/layouts/home.html.slim_spec.rb
@@ -24,11 +24,26 @@ RSpec.describe 'layouts/home', type: :view do
   end
 
   it 'renders sign in form' do
+    cached_sign_in_type = Figaro.env.sign_in_type
+    ENV['SIGN_IN_TYPE'] = 'form'
+
     render
 
     assert_select 'form[action=?][method=?]', user_sign_in_path, 'post' do
       assert_select 'input#name[name=name]'
       assert_select 'input#email[name=email]'
     end
+    ENV['SIGN_IN_TYPE'] = cached_sign_in_type
+  end
+
+  it 'should not renders sign in form when sign in type is not form' do
+    cached_sign_in_type = Figaro.env.sign_in_type
+    ENV['SIGN_IN_TYPE'] = 'not_form'
+
+    render
+
+    assert_select 'form[action=?][method=?]', user_sign_in_path, 'post', 0
+
+    ENV['SIGN_IN_TYPE'] = cached_sign_in_type
   end
 end
