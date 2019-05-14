e87ad3055263ff9168e2660b28d41a38bde4e0f0
William Albertus Dembo <william.dembo@aux.go-jek.com>
2019-05-14 14:51:30 +0700

    home view will render google oauth only if sign in type is google

--- spec/views/layouts/home.html.slim_spec.rb
+++ spec/views/layouts/home.html.slim_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'layouts/home', type: :view do
-  it 'renders google auth link' do
+  it 'should renders google auth link when sign in type google' do
+    cached_sign_in_type = Figaro.env.sign_in_type
+    ENV['SIGN_IN_TYPE'] = 'google'
+
     render
 
     assert_select "a[href$='#{user_google_oauth2_omniauth_authorize_path}']"
+
+    ENV['SIGN_IN_TYPE'] = cached_sign_in_type
+  end
+
+  it 'should not renders google auth link when sign in type not google' do
+    cached_sign_in_type = Figaro.env.sign_in_type
+    ENV['SIGN_IN_TYPE'] = 'not_google'
+
+    render
+
+    assert_select "a[href$='#{user_google_oauth2_omniauth_authorize_path}']", 0
+
+    ENV['SIGN_IN_TYPE'] = cached_sign_in_type
   end
 
   it 'renders sign in form' do
