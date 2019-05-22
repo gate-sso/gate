e00f05336831b98c83be2df1d9e9eb504647eb67
William Albertus Dembo <william.dembo@aux.go-jek.com>
2019-05-22 11:11:35 +0700

    cleanup rubocop for vpns controller

--- app/controllers/vpns_controller.rb
+++ app/controllers/vpns_controller.rb
@@ -1,11 +1,12 @@
 class VpnsController < ApplicationController
   before_action :set_paper_trail_whodunnit
-  before_action :authorize_user, except: [:create_group_associated_users, :show, :index, :user_associated_groups, :group_associated_users, :search]
-  before_action :set_vpn, only: [:show, :edit, :update, :destroy, \
-                                 :user_associated_groups, :add_dns_server, :remove_dns_server, \
-                                 :add_search_domain, :remove_search_domain, \
-                                 :add_supplemental_match_domain, :remove_supplemental_match_domain, \
-                                 :migrate_to_new_group, :assign_group]
+  before_action :authorize_user, except: %i[create_group_associated_users show index
+                                            user_associated_groups group_associated_users search]
+  before_action :set_vpn, only: %i[show edit update destroy
+                                   user_associated_groups add_dns_server remove_dns_server
+                                   add_search_domain remove_search_domain
+                                   add_supplemental_match_domain remove_supplemental_match_domain
+                                   migrate_to_new_group assign_group]
 
   before_action :authenticate_user!
 
@@ -45,47 +46,48 @@ class VpnsController < ApplicationController
   end
 
   def add_dns_server
-    if current_user.admin?
-      domain_name_server = VpnDomainNameServer.find_or_create_by(server_address: params[:server_address], vpn: @vpn) if  params[:server_address].present?
+    if current_user.admin? && params[:server_address].present?
+      VpnDomainNameServer.find_or_create_by(server_address: params[:server_address], vpn: @vpn)
     end
-    redirect_to vpn_path(@vpn, anchor: "dns_hosts")
+    redirect_to vpn_path(@vpn, anchor: 'dns_hosts')
   end
 
   def add_search_domain
-    if current_user.admin?
-      search_domain = VpnSearchDomain.find_or_create_by(search_domain: params[:search_domain], vpn: @vpn) if params[:search_domain].present?
+    if current_user.admin? && params[:search_domain].present?
+      VpnSearchDomain.find_or_create_by(search_domain: params[:search_domain], vpn: @vpn)
     end
-    redirect_to vpn_path(@vpn, anchor: "search_domains")
+    redirect_to vpn_path(@vpn, anchor: 'search_domains')
   end
 
   def add_supplemental_match_domain
-    if current_user.admin?
-      search_domain = VpnSupplementalMatchDomain.find_or_create_by(supplemental_match_domain: params[:supplemental_match_domain], vpn: @vpn) if  params[:supplemental_match_domain].present?
+    if current_user.admin? && params[:supplemental_match_domain].present?
+      VpnSupplementalMatchDomain.find_or_create_by(
+        supplemental_match_domain: params[:supplemental_match_domain],
+        vpn: @vpn
+      )
     end
-    redirect_to vpn_path(@vpn, anchor: "match_domains")
-
+    redirect_to vpn_path(@vpn, anchor: 'match_domains')
   end
 
   def remove_dns_server
     if current_user.admin?
-      vpn_domain_name_server = VpnDomainNameServer.delete(params[:vpn_domain_name_server_id])
+      VpnDomainNameServer.delete(params[:vpn_domain_name_server_id])
     end
-    redirect_to vpn_path(@vpn, anchor: "dns_hosts")
+    redirect_to vpn_path(@vpn, anchor: 'dns_hosts')
   end
 
   def remove_search_domain
     if current_user.admin?
-      search_domain = VpnSearchDomain.delete(params[:vpn_search_domain_id])
+      VpnSearchDomain.delete(params[:vpn_search_domain_id])
     end
-    redirect_to vpn_path(@vpn, anchor: "search_domains")
-
+    redirect_to vpn_path(@vpn, anchor: 'search_domains')
   end
 
   def remove_supplemental_match_domain
     if current_user.admin?
-      search_domain = VpnSupplementalMatchDomain.delete(params[:vpn_supplemental_match_domain_id])
+      VpnSupplementalMatchDomain.delete(params[:vpn_supplemental_match_domain_id])
     end
-    redirect_to vpn_path(@vpn, anchor: "match_domains")
+    redirect_to vpn_path(@vpn, anchor: 'match_domains')
   end
 
   def assign_group
@@ -93,7 +95,7 @@ class VpnsController < ApplicationController
       @vpn.groups.delete_all
       @vpn.groups << Group.where(id: params[:group_id]).first
     end
-    redirect_to vpn_path(@vpn, anchor: "match_domains")
+    redirect_to vpn_path(@vpn, anchor: 'match_domains')
   end
 
   def show
@@ -115,20 +117,23 @@ class VpnsController < ApplicationController
       end
     end
 
-    render "show"
+    render 'show'
   end
 
   def group_associated_users
     @group = Group.find(params[:group_id])
     if current_user.admin? || @group.group_admin.try(:user) == current_user
       @users = @group.users
-      @vpn_group_user_associations = VpnGroupUserAssociation.where(vpn_id: params[:vpn_id], group_id: params[:group_id])
-      @vpn_enabled_users = @vpn_group_user_associations.map { |r| r.user }
+      @vpn_group_user_associations = VpnGroupUserAssociation.where(
+        vpn_id: params[:vpn_id],
+        group_id: params[:group_id]
+      )
+      @vpn_enabled_users = @vpn_group_user_associations.map(&:user)
 
       @vpn_disabled_users = @users - @vpn_enabled_users
 
-      @vpn_enabled_users = @vpn_enabled_users.sort_by{ |user| user.email}
-      @vpn_disabled_users = @vpn_disabled_users.sort_by{ |user| user.email}
+      @vpn_enabled_users = @vpn_enabled_users.sort_by(&:email)
+      @vpn_disabled_users = @vpn_disabled_users.sort_by(&:email)
     end
     respond_to do |format|
       format.json { render status: :ok, json: { enabled: @vpn_enabled_users, disabled: @vpn_disabled_users } }
@@ -137,16 +142,26 @@ class VpnsController < ApplicationController
 
   def create_group_associated_users
     if current_user.admin? ||
-        VpnGroupAssociation.find_by_vpn_id_and_group_id(params[:vpn_id].to_i, params[:group_id]).group.group_admin.user == current_user
+        VpnGroupAssociation.find_by_vpn_id_and_group_id(
+          params[:vpn_id].to_i,
+          params[:group_id]
+        ).group.group_admin.user == current_user
       @users_selected = params[:users] || []
       @associations_made = []
-      VpnGroupUserAssociation.where(vpn_id:params[:vpn_id].to_i, group_id: params[:group_id].to_i).each do |vpn_association|
+      VpnGroupUserAssociation.where(
+        vpn_id: params[:vpn_id].to_i,
+        group_id: params[:group_id].to_i
+      ).each do |vpn_association|
         unless @users_selected.include? vpn_association.user.id
           vpn_association.destroy
         end
       end
       @users_selected.each do |user|
-        @associations_made << VpnGroupUserAssociation.find_or_create_by(vpn_id: params[:vpn_id], group_id: params[:group_id], user_id: user.to_i)
+        @associations_made << VpnGroupUserAssociation.find_or_create_by(
+          vpn_id: params[:vpn_id],
+          group_id: params[:group_id],
+          user_id: user.to_i
+        )
       end
 
       respond_to do |format|
@@ -154,7 +169,7 @@ class VpnsController < ApplicationController
       end
     else
       respond_to do |format|
-        format.json { render status: :unauthorized, json: "not gonna happen" }
+        format.json { render status: :unauthorized, json: 'not gonna happen' }
       end
     end
   end
@@ -166,20 +181,21 @@ class VpnsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to vpns_path, notice: 'Vpn was successfully destroyed.' }
-      format.json { render status: :ok, json: "vpn destroyed" }
+      format.json { render status: :ok, json: 'vpn destroyed' }
     end
   end
 
   def search
     @vpns = Vpn.
-      where("name LIKE ?", "%#{params[:q]}%").
-      order("name ASC").
+      where('name LIKE ?', "%#{params[:q]}%").
+      order('name ASC').
       limit(20)
-    data = @vpns.map{ |vpn| {id: vpn.id, name: vpn.name} }
+    data = @vpns.map { |vpn| { id: vpn.id, name: vpn.name } }
     render json: data
   end
 
   private
+
   def set_vpn
     @vpn = Vpn.find(params[:id])
   end
