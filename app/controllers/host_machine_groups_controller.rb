3d3f9bd86bf24255591fa6c96606ca2c5662d03f
William Albertus Dembo <william.dembo@aux.go-jek.com>
2019-05-22 10:50:24 +0700

    cleanup rubocop for host machine groups controller

--- app/controllers/host_machine_groups_controller.rb
+++ app/controllers/host_machine_groups_controller.rb
@@ -14,6 +14,7 @@ class HostMachineGroupsController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_host_machine
     @host_machine = host_machine.find(params[:id])
