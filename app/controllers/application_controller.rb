d2dc91957014eb2f4ec27f3a40d0bdcb87a0d49a
William Albertus Dembo <william.dembo@aux.go-jek.com>
2019-05-22 10:20:17 +0700

    cleanup rubocop for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def render_404
     respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
+      format.html { render file: "#{Rails.root}/public/404", layout: false, status: :not_found }
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
@@ -21,6 +21,5 @@ class ApplicationController < ActionController::Base
 
   def render_error(errors, status = 400)
     render 'common/errors', locals: { errors: errors }, status: status
-    return
   end
 end
