bb5a0b01685680fe85f0ce187b911ad840873058
William Albertus Dembo <william.dembo@aux.go-jek.com>
2019-05-22 11:04:30 +0700

    cleanup rubocop for vpn domain name servers controller

--- app/controllers/vpn_domain_name_servers_controller.rb
+++ app/controllers/vpn_domain_name_servers_controller.rb
@@ -1,10 +1,6 @@
 class VpnsController < ApplicationController
-
   before_action :authorize_user
-  before_action :set_vpn, only: [:show, :edit, :update, :destroy, :user_associated_groups]
+  before_action :set_vpn, only: %i[show edit update destroy user_associated_groups]
 
-  def destroy
-
-  end
-
+  def destroy; end
 end
