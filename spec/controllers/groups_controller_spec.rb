42eb91879d76854327f8ea7e3856c4d49a3d75c1
William Albertus Dembo <william.dembo@go-jek.com>
2019-06-21 13:19:32 +0700

    cleanup group controller spec
    Co-authored-by: mufti1 <muftiismirizqi@gmail.com>

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GroupsController, type: :controller do
-  let(:product_name) { "product-name"  }
-  let(:user) { FactoryBot.create(:user, name: "foobar", user_login_id: "foobar", email: "foobar@foobar.com")  }
+  let(:product_name) { 'product-name'  }
+  let(:user) { create(:user, name: 'foobar', user_login_id: 'foobar', email: 'foobar@foobar.com') }
   describe 'Search for Groups' do
-    it "should return groups according to supplied search string" do
+    it 'should return groups according to supplied search string' do
       sign_in user
       groups = create_list(:group, 3)
-      get :search, params: { q: "People" }
-      expect(JSON.parse(response.body)).to eq(groups.map{|m| {"id" => m.id, "name" => m.name}})
+      get :search, params: { q: 'People' }
+      expect(JSON.parse(response.body)).to eq(groups.map { |m| { 'id' => m.id, 'name' => m.name } })
     end
   end
 end
