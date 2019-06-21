e4f41eeb53c00ef0521170ab74270fe588b37b37
William Albertus Dembo <william.dembo@go-jek.com>
2019-06-21 13:42:41 +0700

    add group controller spec get index when unauthenticated should return status 302
    Co-authored-by: mufti1 <muftiismirizqi@gmail.com>

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -3,6 +3,17 @@ require 'rails_helper'
 RSpec.describe GroupsController, type: :controller do
   let(:product_name) { 'product-name'  }
   let(:user) { create(:user, name: 'foobar', user_login_id: 'foobar', email: 'foobar@foobar.com') }
+
+  describe 'GET #index' do
+    context 'unauthenticated' do
+      it 'should return 302' do
+        get :index
+
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
+
   describe 'Search for Groups' do
     it 'should return groups according to supplied search string' do
       sign_in user
