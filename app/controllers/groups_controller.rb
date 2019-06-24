ea6ab2fd8496ee474b675bad16b938ed98b6bedf
William Albertus Dembo <william.dembo@go-jek.com>
2019-06-24 13:49:06 +0700

    refactor groups_controller#add_group to decrease variables scope
    Co-authored-by: mufti1 <muftiismirizqi@gmail.com>

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -150,15 +150,15 @@ class GroupsController < ApplicationController
   end
 
   def add_group
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
     if current_user.admin?
       begin
         expiration_date = expiration_date_from_param
       rescue ArgumentError
         response_message = 'Expiration date is wrong'
       end
-      @group = Group.find(params[:group_id])
-      @group.add_user_with_expiration(@user.id, expiration_date)
+      group = Group.find(params[:group_id])
+      group.add_user_with_expiration(user.id, expiration_date)
     end
     redirect_to user_path, notice: response_message
   end
