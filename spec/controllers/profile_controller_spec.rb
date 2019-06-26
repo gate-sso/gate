aea953b6f05046f9a5bde8f957987999ab19b29c
William Albertus Dembo <william.dembo@go-jek.com>
2019-06-26 11:19:04 +0700

    add profile controller spec post update when authenticated as admin should redirect to user path after update

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -1,11 +1,23 @@
 describe ProfileController, type: :controller do
+  let!(:admin) { create(:admin_user) }
   let(:user) { create(:user) }
+
   describe '#update' do
     context 'unauthenticated' do
       it 'should return status 302' do
         post :update, params: { id: user.id }
 
         expect(response).to have_http_status(302)
+      end
+    end
+
+    context 'authenticated as admin' do
+      it 'should redirect to user_path after update' do
+        sign_in admin
+
+        post :update, params: { id: user.id, user: { admin: false, active: false } }
+
+        expect(response).to redirect_to(user_path)
       end
     end
   end
