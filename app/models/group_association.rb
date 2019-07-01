d11f7e4d225668185804735ecf828315bcac971c
William Albertus Dembo <william.dembo@go-jek.com>
2019-07-01 13:11:21 +0700

    group association model revoke_expired should should revoke associations that expired yesterday
    Co-authored-by: mufti1 <muftiismirizqi@gmail.com>

--- app/models/group_association.rb
+++ app/models/group_association.rb
@@ -2,4 +2,8 @@ class GroupAssociation < ApplicationRecord
   has_paper_trail
   belongs_to :user
   belongs_to :group
+
+  def self.revoke_expired
+    delete_all
+  end
 end
--- spec/models/group_association_spec.rb
+++ spec/models/group_association_spec.rb
@@ -1,4 +1,14 @@
-require 'rails_helper'
+describe GroupAssociation, type: :model do
+  describe '.revoke_expired' do
+    it 'should revoke associations that expired yesterday' do
+      user = create(:user)
+      group = create(:group)
+      group.add_user_with_expiration(user, Date.today - 1)
 
-RSpec.describe GroupAssociation, type: :model do
+      GroupAssociation.revoke_expired
+
+      expired_association = GroupAssociation.where('expiration_date < ?', Date.today).count
+      expect(expired_association).to eq 0
+    end
+  end
 end
