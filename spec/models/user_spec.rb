0c67be3094266c8d8c05a8678f4c4dabfe0d7564
William Albertus Dembo <william.dembo@go-jek.com>
2019-07-03 17:06:52 +0700

    user model get expiration date from group should return user expiration date
    Co-authored-by: mufti1 <muftiismirizqi@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -257,6 +257,19 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe '#expiration_date_from' do
+    it 'should return expiration date from group' do
+      user = create(:user)
+      group = create(:group)
+      expiration_date = Date.parse('2019-10-20')
+      group.add_user_with_expiration(user.id, expiration_date)
+
+      user_expiration_date = user.expiration_date_from group.id
+
+      expect(user_expiration_date).to eq(expiration_date)
+    end
+  end
 end
 
 UID_CONSTANT = 5000
