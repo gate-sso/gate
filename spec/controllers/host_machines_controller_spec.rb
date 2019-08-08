48f34cf3eaf8d40a3b2932ad0bb707b1d1854c88
William Albertus Dembo <william.dembo@go-jek.com>
2019-08-08 13:14:19 +0700

    refactor host machine controller spec
    Co-authored-by: ralibi <raymondralibi@gmail.com>

--- spec/controllers/host_machines_controller_spec.rb
+++ spec/controllers/host_machines_controller_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe HostMachinesController, type: :controller do
-
-  let(:user) { FactoryBot.create(:user, name: "foobar", user_login_id: "foobar", email: "foobar@foobar.com")  }
+  let(:user) do
+    create(:user, name: 'foobar', user_login_id: 'foobar', email: 'foobar@foobar.com')
+  end
   describe 'Search for Hosts' do
-    it "should return hosts according to supplied search string" do
+    it 'should return hosts according to supplied search string' do
       sign_in user
       host_machines = create_list(:host_machine, 3)
-      get :search, params: { q: "host" }
-      expect(JSON.parse(response.body)).to eq(host_machines.map{|m| {"id" => m.id, "name" => m.name}})
+      get :search, params: { q: 'host' }
+      expect(response.body).to eq(host_machines.map { |m| { id: m.id, name: m.name } }.to_json)
     end
   end
 end
