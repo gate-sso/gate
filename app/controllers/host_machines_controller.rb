d26ed5bfc72affdc86d2dcce8eea2bf827bc4c96
William Albertus Dembo <william.dembo@go-jek.com>
2019-08-08 15:36:47 +0700

    refactor host machines controller to extract method for authorize user
    Co-authored-by: ralibi <raymondralibi@gmail.com>

--- app/controllers/host_machines_controller.rb
+++ app/controllers/host_machines_controller.rb
@@ -3,6 +3,8 @@ class HostMachinesController < ApplicationController
   before_action :set_host_machine, only: %i[add_group show edit update destroy delete_group]
   prepend_before_action :setup_user if Rails.env.development?
   before_action :authenticate_user!
+  before_action :authorize_user, only: %i[delete_group update]
+
   def index
     @title = 'Host'
     @host_machines = HostMachine.all
@@ -34,10 +36,6 @@ class HostMachinesController < ApplicationController
   end
 
   def update
-    unless current_user.admin?
-      return redirect_to host_machines_path, notice: 'Unauthorized access'
-    end
-
     @host_machine.update(default_admins: params[:host_machine][:default_admins])
     redirect_to host_machine_path @host_machine
   end
@@ -58,10 +56,6 @@ class HostMachinesController < ApplicationController
   end
 
   def delete_group
-    unless current_user.admin?
-      return redirect_to host_machines_path, notice: 'Unauthorized access'
-    end
-
     group = Group.find(params[:group_id])
     @host_machine.groups.delete(group)
     @host_machine.save!
@@ -87,5 +81,11 @@ class HostMachinesController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def host_machine_params
     params.require(:host_machine).permit(:name)
+  end
+
+  def authorize_user
+    unless current_user.admin?
+      redirect_to host_machines_path, notice: 'Unauthorized access'
+    end
   end
 end
