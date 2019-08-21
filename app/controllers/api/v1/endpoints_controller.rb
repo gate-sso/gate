6fa11789cc1b3be489a52063668b2a238997fac2
William Albertus Dembo <william.dembo@go-jek.com>
2019-08-21 10:28:42 +0700

    post create endpoints when given valid params should return http status 200

---
+++ app/controllers/api/v1/endpoints_controller.rb
@@ -0,0 +1,5 @@
+class ::Api::V1::EndpointsController < ::Api::V1::BaseController
+  def create
+    render json: {}
+  end
+end
---
+++ spec/controllers/api/v1/endpoints_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe ::Api::V1::EndpointsController, type: :controller do
+  let(:valid_attributes) do
+    {
+      path: '/',
+      method: 'GET',
+    }
+  end
+
+  describe '#create' do
+    context 'given valid param' do
+      it 'should return http status 200' do
+        admin = build(:admin_user)
+        admin.access_token = build(:access_token)
+        admin.save
+        post :create, params: { endpoint: valid_attributes, access_token: admin.access_token.token }
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
