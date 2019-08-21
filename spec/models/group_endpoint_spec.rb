4374a96bd0d4d82c4b1d75ac922ca1f37d0f9a38
William Albertus Dembo <william.dembo@go-jek.com>
2019-08-21 16:27:26 +0700

    refactor group endpoint models to extract variables

--- spec/models/group_endpoint_spec.rb
+++ spec/models/group_endpoint_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe GroupEndpoint, type: :model do
+  let(:group) { create(:group) }
+  let(:endpoint) { create(:endpoint) }
+
   describe 'validations' do
     context 'given duplicate group with endpoint' do
       it 'should not valid' do
-        group = create(:group)
-        endpoint = create(:endpoint)
         GroupEndpoint.create(group: group, endpoint: endpoint)
         group_endpoint = GroupEndpoint.new(group: group, endpoint: endpoint)
         expect(group_endpoint).not_to be_valid
@@ -14,7 +15,6 @@ describe GroupEndpoint, type: :model do
 
     context 'given nil group' do
       it 'should not valid' do
-        endpoint = create(:endpoint)
         group_endpoint = GroupEndpoint.new(group: nil, endpoint: endpoint)
         expect(group_endpoint).not_to be_valid
       end
@@ -22,7 +22,6 @@ describe GroupEndpoint, type: :model do
 
     context 'given nil endpoint' do
       it 'should not valid' do
-        group = create(:group)
         group_endpoint = GroupEndpoint.new(group: group, endpoint: nil)
         expect(group_endpoint).not_to be_valid
       end
