e103eafe9009883bdd49dbc66a7008e890874be1
William Albertus Dembo <william.dembo@go-jek.com>
2019-08-21 11:48:18 +0700

    post create endpoints when authenticated should return http status 401

--- spec/controllers/api/v1/endpoints_controller_spec.rb
+++ spec/controllers/api/v1/endpoints_controller_spec.rb
@@ -16,43 +16,52 @@ describe ::Api::V1::EndpointsController, type: :controller do
   end
 
   describe '#create' do
-    context 'given valid param' do
-      it 'should return http status 200' do
-        post :create, params: { endpoint: valid_attributes, access_token: @admin_token }
-        expect(response).to have_http_status(200)
+    context 'authenticated as admin' do
+      context 'given valid param' do
+        it 'should return http status 200' do
+          post :create, params: { endpoint: valid_attributes, access_token: @admin_token }
+          expect(response).to have_http_status(200)
+        end
+
+        it 'should create endpoint' do
+          post :create, params: { endpoint: valid_attributes, access_token: @admin_token }
+          endpoint = Endpoint.find_by(valid_attributes)
+          expect(endpoint).not_to be nil
+        end
+
+        it 'should return proper response' do
+          post :create, params: { endpoint: valid_attributes, access_token: @admin_token }
+          endpoint = Endpoint.find_by(valid_attributes)
+          expect(response.body).to eq({
+            id: endpoint.id,
+            path: endpoint.path,
+            method: endpoint.method,
+          }.to_json)
+        end
       end
 
-      it 'should create endpoint' do
-        post :create, params: { endpoint: valid_attributes, access_token: @admin_token }
-        endpoint = Endpoint.find_by(valid_attributes)
-        expect(endpoint).not_to be nil
-      end
+      context 'given invalid param' do
+        it 'should return http status 422' do
+          invalid_attributes = valid_attributes
+          invalid_attributes[:method] = 'JUMP'
+          post :create, params: { endpoint: invalid_attributes, access_token: @admin_token }
+          expect(response).to have_http_status(422)
+        end
 
-      it 'should return proper response' do
-        post :create, params: { endpoint: valid_attributes, access_token: @admin_token }
-        endpoint = Endpoint.find_by(valid_attributes)
-        expect(response.body).to eq({
-          id: endpoint.id,
-          path: endpoint.path,
-          method: endpoint.method,
-        }.to_json)
+        it 'should return proper response' do
+          invalid_attributes = valid_attributes
+          invalid_attributes[:method] = 'JUMP'
+          post :create, params: { endpoint: invalid_attributes, access_token: @admin_token }
+          response_body = JSON.parse response.body
+          expect(response_body['status']).to include('method')
+        end
       end
     end
 
-    context 'given invalid param' do
-      it 'should return http status 422' do
-        invalid_attributes = valid_attributes
-        invalid_attributes[:method] = 'JUMP'
-        post :create, params: { endpoint: invalid_attributes, access_token: @admin_token }
-        expect(response).to have_http_status(422)
-      end
-
-      it 'should return proper response' do
-        invalid_attributes = valid_attributes
-        invalid_attributes[:method] = 'JUMP'
-        post :create, params: { endpoint: invalid_attributes, access_token: @admin_token }
-        response_body = JSON.parse response.body
-        expect(response_body['status']).to include('method')
+    context 'unauthenticated' do
+      it 'should return http status 401' do
+        post :create, params: { endpoint: valid_attributes }
+        expect(response.status).to eq(401)
       end
     end
   end
