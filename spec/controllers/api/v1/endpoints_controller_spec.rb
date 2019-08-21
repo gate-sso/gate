afb6687bf452683e217c8135b1ed8e5c6a414843
William Albertus Dembo <william.dembo@go-jek.com>
2019-08-21 11:39:47 +0700

    refactor endpoints api controller spec to extract admin token variable

--- spec/controllers/api/v1/endpoints_controller_spec.rb
+++ spec/controllers/api/v1/endpoints_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe ::Api::V1::EndpointsController, type: :controller do
-  let(:admin) do
-    admin = build(:admin_user)
-    admin.access_token = build(:access_token)
-    admin.save!
-    admin
+  before(:each) do
+    @admin = build(:admin_user)
+    @admin.access_token = build(:access_token)
+    @admin.save!
+    @admin_token = @admin.access_token.token
   end
+
   let(:valid_attributes) do
     {
       path: '/',
@@ -17,18 +18,18 @@ describe ::Api::V1::EndpointsController, type: :controller do
   describe '#create' do
     context 'given valid param' do
       it 'should return http status 200' do
-        post :create, params: { endpoint: valid_attributes, access_token: admin.access_token.token }
+        post :create, params: { endpoint: valid_attributes, access_token: @admin_token }
         expect(response).to have_http_status(200)
       end
 
       it 'should create endpoint' do
-        post :create, params: { endpoint: valid_attributes, access_token: admin.access_token.token }
+        post :create, params: { endpoint: valid_attributes, access_token: @admin_token }
         endpoint = Endpoint.find_by(valid_attributes)
         expect(endpoint).not_to be nil
       end
 
       it 'should return proper response' do
-        post :create, params: { endpoint: valid_attributes, access_token: admin.access_token.token }
+        post :create, params: { endpoint: valid_attributes, access_token: @admin_token }
         endpoint = Endpoint.find_by(valid_attributes)
         expect(response.body).to eq({
           id: endpoint.id,
@@ -42,14 +43,14 @@ describe ::Api::V1::EndpointsController, type: :controller do
       it 'should return http status 422' do
         invalid_attributes = valid_attributes
         invalid_attributes[:method] = 'JUMP'
-        post :create, params: { endpoint: invalid_attributes, access_token: admin.access_token.token }
+        post :create, params: { endpoint: invalid_attributes, access_token: @admin_token }
         expect(response).to have_http_status(422)
       end
 
       it 'should return proper response' do
         invalid_attributes = valid_attributes
         invalid_attributes[:method] = 'JUMP'
-        post :create, params: { endpoint: invalid_attributes, access_token: admin.access_token.token }
+        post :create, params: { endpoint: invalid_attributes, access_token: @admin_token }
         response_body = JSON.parse response.body
         expect(response_body['status']).to include('method')
       end
