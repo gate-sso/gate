b699f75a31c62b1c1e116f44412cdf8e197aa4f3
William Albertus Dembo <william.dembo@go-jek.com>
2019-08-21 11:21:47 +0700

    refactor endpoints api controller to extract admin variable

--- spec/controllers/api/v1/endpoints_controller_spec.rb
+++ spec/controllers/api/v1/endpoints_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe ::Api::V1::EndpointsController, type: :controller do
+  let(:admin) do
+    admin = build(:admin_user)
+    admin.access_token = build(:access_token)
+    admin.save!
+    admin
+  end
   let(:valid_attributes) do
     {
       path: '/',
@@ -11,26 +17,17 @@ describe ::Api::V1::EndpointsController, type: :controller do
   describe '#create' do
     context 'given valid param' do
       it 'should return http status 200' do
-        admin = build(:admin_user)
-        admin.access_token = build(:access_token)
-        admin.save
         post :create, params: { endpoint: valid_attributes, access_token: admin.access_token.token }
         expect(response).to have_http_status(200)
       end
 
       it 'should create endpoint' do
-        admin = build(:admin_user)
-        admin.access_token = build(:access_token)
-        admin.save
         post :create, params: { endpoint: valid_attributes, access_token: admin.access_token.token }
         endpoint = Endpoint.find_by(valid_attributes)
         expect(endpoint).not_to be nil
       end
 
       it 'should return proper response' do
-        admin = build(:admin_user)
-        admin.access_token = build(:access_token)
-        admin.save
         post :create, params: { endpoint: valid_attributes, access_token: admin.access_token.token }
         endpoint = Endpoint.find_by(valid_attributes)
         expect(response.body).to eq({
