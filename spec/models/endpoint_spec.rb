5347034bb1595aefb88661e5e5d3c815b539344e
William Albertus Dembo <william.dembo@go-jek.com>
2019-08-22 13:16:40 +0700

    refactor endpoint spec to group on describe

--- spec/models/endpoint_spec.rb
+++ spec/models/endpoint_spec.rb
@@ -2,45 +2,49 @@ require 'rails_helper'
 
 describe Endpoint, type: :model do
   describe 'validations' do
-    context 'when given nil path' do
-      it 'should not valid' do
-        endpoint = build(:endpoint, path: nil)
-        expect(endpoint).not_to be_valid
+    describe 'path' do
+      context 'when given nil path' do
+        it 'should not valid' do
+          endpoint = build(:endpoint, path: nil)
+          expect(endpoint).not_to be_valid
+        end
+      end
+
+      context 'when given path with parameter' do
+        it 'should valid' do
+          endpoint = build(:endpoint, path: '/users/:id')
+          expect(endpoint).to be_valid
+        end
+      end
+
+      context 'when given invalid path' do
+        it 'should not valid' do
+          endpoint = build(:endpoint, path: '/users/::id')
+          expect(endpoint).not_to be_valid
+        end
+      end
+
+      context 'when given path ended with /' do
+        it 'should not valid' do
+          endpoint = build(:endpoint, path: '/users/')
+          expect(endpoint).not_to be_valid
+        end
       end
     end
 
-    context 'when given path with parameter' do
-      it 'should valid' do
-        endpoint = build(:endpoint, path: '/users/:id')
-        expect(endpoint).to be_valid
+    describe 'method' do
+      context 'when given nil method' do
+        it 'should not valid' do
+          endpoint = build(:endpoint, method: nil)
+          expect(endpoint).not_to be_valid
+        end
       end
-    end
 
-    context 'when given invalid path' do
-      it 'should not valid' do
-        endpoint = build(:endpoint, path: '/users/::id')
-        expect(endpoint).not_to be_valid
-      end
-    end
-
-    context 'when given path ended with /' do
-      it 'should not valid' do
-        endpoint = build(:endpoint, path: '/users/')
-        expect(endpoint).not_to be_valid
-      end
-    end
-
-    context 'when given nil method' do
-      it 'should not valid' do
-        endpoint = build(:endpoint, method: nil)
-        expect(endpoint).not_to be_valid
-      end
-    end
-
-    context 'when given unknown method' do
-      it 'should not valid' do
-        endpoint = build(:endpoint, method: 'JUMP')
-        expect(endpoint).not_to be_valid
+      context 'when given unknown method' do
+        it 'should not valid' do
+          endpoint = build(:endpoint, method: 'JUMP')
+          expect(endpoint).not_to be_valid
+        end
       end
     end
   end
