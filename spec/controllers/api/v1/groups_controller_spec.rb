ba19d5a842d7d408de1722c4ee6e6aa9c461f99a
William Albertus Dembo <william.dembo@go-jek.com>
2019-09-23 13:01:37 +0700

    refactor group controller api spec to rename admin variables

--- spec/controllers/api/v1/groups_controller_spec.rb
+++ spec/controllers/api/v1/groups_controller_spec.rb
@@ -6,24 +6,24 @@ RSpec.describe ::Api::V1::GroupsController, type: :controller do
   end
 
   before(:each) do
-    @user = build(:user)
-    @user.access_token = build(:access_token)
-    @user.save
-    @token = @user.access_token.token
+    @admin = build(:admin_user)
+    @admin.access_token = build(:access_token)
+    @admin.save
+    @admin_token = @admin.access_token.token
   end
 
   describe '#create' do
     context 'authenticated as admin' do
       context 'with valid_attributes' do
         it 'should create groups' do
-          post :create, params: { group: valid_attributes, access_token: @token }
+          post :create, params: { group: valid_attributes, access_token: @admin_token }
           group = Group.where(name: valid_attributes[:name]).first
           expect(group.blank?).to eq(false)
           expect(group.name).to eq(valid_attributes[:name])
         end
 
         it 'should return proper response' do
-          post :create, params: { group: valid_attributes, access_token: @token }
+          post :create, params: { group: valid_attributes, access_token: @admin_token }
           expect(response.status).to eq(200)
           group = Group.where(name: valid_attributes[:name]).first
           obj = JSON.parse(response.body)
@@ -35,7 +35,7 @@ RSpec.describe ::Api::V1::GroupsController, type: :controller do
       context 'group already exist' do
         it 'should return existing group id' do
           existing_group = create(:group, name: valid_attributes[:name])
-          post :create,  params: { group: valid_attributes, access_token: @token }
+          post :create,  params: { group: valid_attributes, access_token: @admin_token }
           expect(response.status).to eq(422)
           expect(response.body).to eq({
             status: 'group already exist',
@@ -65,7 +65,7 @@ RSpec.describe ::Api::V1::GroupsController, type: :controller do
           post :add_user, params: {
             id: @group.id,
             user_id: @new_user.id,
-            access_token: @token,
+            access_token: @admin_token,
           }
           expect(response.status).to eq 204
         end
@@ -74,7 +74,7 @@ RSpec.describe ::Api::V1::GroupsController, type: :controller do
           post :add_user, params: {
             id: @group.id,
             user_id: @new_user.id,
-            access_token: @token,
+            access_token: @admin_token,
           }
           expect(@group.users).to contain_exactly @new_user
         end
