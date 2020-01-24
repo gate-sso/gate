e986451092d222004f98e8e4a652413de04cc5d1
William Albertus Dembo <william.dembo@go-jek.com>
2020-01-24 11:52:49 +0700

    GroupAssociation.revoke_expired should create paper trail with event destroy when found expired associations

--- spec/models/group_association_spec.rb
+++ spec/models/group_association_spec.rb
@@ -1,19 +1,33 @@
+require 'rails_helper'
+
 describe GroupAssociation, type: :model do
   describe '.revoke_expired' do
-    it 'should revoke associations that expired yesterday' do
-      user = create(:user)
-      group = create(:group)
-      group.add_user_with_expiration(user, Date.today - 1)
+    let(:user) { create(:user) }
+    let(:group) { create(:group) }
 
-      GroupAssociation.revoke_expired
+    context 'when found expired associations' do
+      it 'should revoke associations' do
+        group.add_user_with_expiration(user, Date.today - 1)
 
-      expired_association = GroupAssociation.where('expiration_date < ?', Date.today).count
-      expect(expired_association).to eq 0
+        GroupAssociation.revoke_expired
+
+        expired_association = GroupAssociation.where('expiration_date < ?', Date.today).count
+        expect(expired_association).to eq 0
+      end
+
+      it 'should create paper trail with event destroy' do
+        group_association = group.add_user_with_expiration(user, Date.today - 1)
+
+        GroupAssociation.revoke_expired
+
+        versions = PaperTrail::Version.
+          with_item_keys(GroupAssociation.name, group_association.id).
+          where(event: 'destroy')
+        expect(versions.length).to eq(1)
+      end
     end
 
     it 'should not revoke associations that not expired yet' do
-      user = create(:user)
-      group = create(:group)
       group.add_user_with_expiration(user, Date.today)
 
       GroupAssociation.revoke_expired
